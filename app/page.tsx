fde92e4a817e02c2662eeedce2b6ce50b0ddc3b2
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2023-11-27 01:25:46 +0900

    window참조시ReferenceError: window is not defined 에러 이슈 해결을 위하 동적컴포넌트로 import방식 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
-import Booking from "@/components/Booking/Booking";
+import dynamic from "next/dynamic";
 import Image from "next/image";
+
+const Booking = dynamic(() => import("@/components/Booking/Booking"), {
+  ssr: false,
+}); //ReferenceError: window is not defined 에러 이슈 수정
 
 export default function Home() {
   return (
     <div>
       <div className="grid  grid-cols-1 md:grid-cols-3">
-        {" "}
         <div className="">
           <Booking />
         </div>
--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import AutoCompleteAdress from "./AutoCompleteAdress";
 
 type Props = {};
 
 const Booking = (props: Props) => {
-  const screenHeight = window.innerHeight * 0.72; //스크롤업애기위해 일정 비율 곱하기
+  useEffect(() => {
+    const innerHeight = window.innerHeight * 0.72;
+  }, []);
+  //const screenHeight = window.innerHeight * 0.72; //스크롤업애기위해 일정 비율 곱하기
   return (
     <div className="p-5">
       <h2 className="text-[20px] font-semibold">Booking</h2>
       <div
         className="border-[1px] p-5 rounded-md"
-        style={{ height: screenHeight }}
+        style={{ height: innerHeight }}
       >
         <AutoCompleteAdress />
         {/**이부분에 MapBox search api implement */}
