1bab8e6710eb0b13762190d1a1724b34b4a45931
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2023-11-27 00:49:50 +0900

    전체 메인 레이아웃 설정

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="grid grid-cols-3">
+      <div className="grid  grid-cols-1 md:grid-cols-3">
         {" "}
         <div className="bg-blue-100">Booking</div>
-        <div className="col-span-2 bg-red-100"> Map</div>
+        <div className="col-span-2 bg-red-100 order-first md:order-last">
+          {" "}
+          Map
+        </div>
       </div>
     </div>
   );
