9250977780e9d21cecb93d6b64722e75e98a1763
ryoon-with-wisdomtrees <ryoon.with.wisdomtrees@gmail.com>
2023-11-27 19:15:10 +0900

    CarList

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,42 @@
 "use client";
 import dynamic from "next/dynamic";
 import Image from "next/image";
-
+import { useEffect, useState } from "react";
 const Booking = dynamic(() => import("@/components/Booking/Booking"), {
   ssr: false,
 }); //ReferenceError: window is not defined 에러 이슈 수정
+import { DestinationCordiContext } from "@/context/DestinationCordiContext";
+import { DirectionDataContext } from "@/context/DirectionDataContext";
+import { SourceCordiContext } from "@/context/SourceCordiContext";
+import { UserLocationContext } from "@/context/UserLocationContext";
 
 export default function Home() {
+  const [userLocation, setUserLocation] = useState<any>();
+  const [soruceCordinates, setSourceCordinates] = useState<any>([]);
+  const [destinationCordinates, setDestinationCordinates] = useState<any>([]);
+  const [directionData, setDirectionData] = useState<any>([]);
   return (
-    <div>
-      <div className="grid  grid-cols-1 md:grid-cols-3">
-        <div className="">
-          <Booking />
-        </div>
-        <div className="col-span-2">Map</div>
-      </div>
+    <div className="">
+      <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+        <SourceCordiContext.Provider
+          value={{ soruceCordinates, setSourceCordinates }}
+        >
+          <DestinationCordiContext.Provider
+            value={{ destinationCordinates, setDestinationCordinates }}
+          >
+            <DirectionDataContext.Provider
+              value={{ directionData, setDirectionData }}
+            >
+              <div className="grid grid-cols-1 md:grid-cols-3">
+                <div className="">
+                  <Booking />
+                </div>
+                <div className="col-span-2">Map</div>
+              </div>
+            </DirectionDataContext.Provider>
+          </DestinationCordiContext.Provider>
+        </SourceCordiContext.Provider>
+      </UserLocationContext.Provider>
     </div>
   );
 }
